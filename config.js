d02fd860888276af207fb1c47bac2115e1c807e9
Jeff Lin <fishtailwrinkle@gmail.com>
2018-05-19 20:44:19 -0700

    Edited CLIENT_ORIGIN

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://connect-the-dots.netlify.com';
+exports.CLIENT_ORIGIN = 'https://connect-the-dots.netlify.com';
 exports.PORT = process.env.PORT || 8080;
