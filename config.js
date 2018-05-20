da59aa87bb55c7e0a573de3e502601b0bf41d041
Jeff Lin <fishtailwrinkle@gmail.com>
2018-05-20 10:15:41 -0700

    Hard coded DATABASE_URL

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL;
+//exports.DATABASE_URL = process.env.DATABASE_URL || mongodb://<dbuser>:<dbpassword>@ds129670.mlab.com:29670/connectthedots;
+exports.DATABASE_URL = 'mongodb://user:password@ds129670.mlab.com:29670/connectthedots';
 exports.CLIENT_ORIGIN = 'https://connect-the-dots.netlify.com';
 exports.PORT = process.env.PORT || 8080;
