b67ef9ab09ebfe083c4d6b4af5d7776c1e680d6c
Jeff Lin <fishtailwrinkle@gmail.com>
2018-05-19 20:36:51 -0700

    Edited CLIENT_ORIGIN in config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/newtest';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://jolly-einstein-d1f729.netlify.com';
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://connect-the-dots.netlify.com';
 exports.PORT = process.env.PORT || 8080;
